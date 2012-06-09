6f89b9e1cac9bd0261eb7ada9df3e868f3563925
David Hassan <assasin665@hotmail.com>
2012-06-09 17:17:23 -0400

    Add the rest of the limbs in Robot

--- Source/Robot.py
+++ Source/Robot.py
@@ -17,21 +17,42 @@ class Robot(NodePath):
             
         point = Point3()
         point.set(0,0,5)
-        self.body = BodyPart(point, "../Models/body","body",render)
+        self.body = BodyPart(point,"../Models/robotbody","body",render,.5,.5,.5)
         #base.cTrav.addCollider( self.body.cnodePath, base.event)
         
-        place = self.body.node.find("**/arm2")
-        print place
+        leftArmPlace = self.body.node.find("**/leftarm")
+        if(leftArmPlace.isEmpty()):
+            raise Exception("Left arm was not loaded properly")
+        rightArmPlace = self.body.node.find("**/rightarm")
+        if(rightArmPlace.isEmpty()):
+            raise Exception("Right arm was not loaded properly")
+        headPlace = self.body.node.find("**/head")
+        if(headPlace.isEmpty()):
+            raise Exception("The head was not loaded properly")
+        leftLegPlace = self.body.node.find("**/leftleg")
+        if(leftLegPlace.isEmpty()):
+            raise Exception("Left leg was not loaded properly")
+        rightLegPlace = self.body.node.find("**/rightleg")
+        if(rightLegPlace.isEmpty()):
+            raise Exception("Right leg was not loaded properly")
+
+
+
         #point.set(0,0,self.body.size[2])
         #self.head = BodyPart(point, "../Models/robothead","head",self.body.node)
         #base.cTrav.addCollider( self.head.cnodePath, base.event)
 
         self.arms = []
         point.set(0,0,0)
-        self.arms.append(BodyPart(point, "../Models/arm", "arm", place) )
-        tag = self.arms[0].node.find("**/arm")
-        pos = self.arms[0].node.getRelativePoint(tag, point )
-        self.arms[0].node.setPos(-pos.getX(), -pos.getY(), -pos.getZ() )
+        self.arms.append(BodyPart(point, "../Models/robotarm","leftarm",leftArmPlace))
+        self.arms.append(BodyPart(point,"../Models/robotarm","rightarm",rightArmPlace))
+
+        self.legs =[]
+        self.legs.append(BodyPart(point, "../Models/robotleg","leftleg",leftLegPlace))
+        self.legs.append(BodyPart(point,"../Models/robotleg","rightleg",rightLegPlace))
+
+        self.head = []
+        self.head.append(BodyPart(point,"../Models/robothead","head",headPlace))
         #point.set(-self.body.size[0] / 2,0,0)
         #self.arms.append(BodyPart(point, "../Models/robotarm","arm",self.body.node ) )
         #base.cTrav.addCollider( self.arms[0].cnodePath, base.event)
